f67b44ab8c33d40454639573fba044093f1e0c92
bhoomika-dudhat <bhoomidudhat01@gmail.com>
2024-05-10 18:01:05 +0530

    after changes

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
    path('index_page/',views.index_page),
-   path('predictor_page/',views.predictor_page,name='predictor_page')
+   path('predictor_page/',views.predictor_page,name='predictor_page'),
+   path('after_rainy_page/',views.after_rainy_page,name='after_rainy_page')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,18 @@ def index_page(request):
 
 def predictor_page(request):
     return render(request,'predictor.html')
+
+
+
+def after_rainy_page(request):
+    if request.method == 'POST':
+        nitrogen = int(request.POST.get('Nitrogen'))
+        phosphorus = int(request.POST.get('Phosphorus'))
+        if 1 <= nitrogen and phosphorus <= 20:
+            result = 1
+        else:
+            result = 2
+    else:
+        result = 3
+
+    return render(request, 'after_rainy.html', {'result': result})
