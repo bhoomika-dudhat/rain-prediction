dbf3093dd71e60b21e78d7a0c6e0c4a1bd2671d9
bhalalaparas <parasbhalala77@gmail.com>
2024-05-11 23:22:49 +0530

    load model in views file

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+import numpy as np
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,15 +13,39 @@ def predictor_page(request):
 
 
 
+crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+model = pickle.load(open(r'myapp/static/model.pkl','rb'))
+
 def after_rainy_page(request):
     if request.method == 'POST':
-        nitrogen = int(request.POST.get('Nitrogen'))
-        phosphorus = int(request.POST.get('Phosphorus'))
-        if 1 <= nitrogen and phosphorus <= 20:
-            result = 1
-        else:
-            result = 2
-    else:
-        result = 3
+        nitrogen = float(request.POST.get('Nitrogen'))
+        phosphorus = float(request.POST.get('Phosphorus'))
+        potessium = float(request.POST.get('Potessium'))
+        temperature = float(request.POST.get('Temperature'))
+        humidity = float(request.POST.get('Humidity'))
+        pH = float(request.POST.get('pH'))
+        rainfall = float(request.POST.get('Rainfall'))
+        feature_list =  np.array([[nitrogen, phosphorus, potessium, temperature, humidity, pH, rainfall]])
+        prediction = model.predict(feature_list).reshape(1,-1)
 
-    return render(request, 'after_rainy.html', {'result': result})
+        print("prediction:::::::::::: ",prediction)
+
+        result = prediction[0][0]
+        if result in crop_dict:
+            crop = crop_dict[result]
+            print("{} is a best crop to be cultivated ".format(crop))
+        else:
+            print("Sorry are not able to recommend a proper crop for this environment")        
+                        
+    #     if 1 <= nitrogen and phosphorus <= 20:
+    #         result = 1
+    #     else:
+    #         result = 2
+    # else:
+    #     result = 3
+
+    return render(request, 'after_rainy.html', {'result': result, 'crop_recommendation': crop})
