7a0c2bae85eb9c1184b33ef7f653be69254add41
bhoomika-dudhat <bhoomidudhat01@gmail.com>
2024-05-14 15:11:00 +0530

    after changes

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   path('index_page/',views.index_page),
+   path('',views.index_page),
    path('predictor_page/',views.predictor_page,name='predictor_page'),
    path('after_rainy_page/',views.after_rainy_page,name='after_rainy_page')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
